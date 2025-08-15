46dc758eb8e9f6c275f938cf18f25656e99d3073
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-08-15 19:20:06 +0600

    rectangle_area_perimeter.cpp

---
+++ rectangle_area_perimeter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float length, width, area, perimeter;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+    cout << "Enter width: ";
+    cin >> width;
+
+
+    area = length * width;
+
+   
+    perimeter = 2 * (length + width);
+
+    cout << "Area of Rectangle = " << area << endl;
+    cout << "Perimeter of Rectangle = " << perimeter << endl;
+
+    return 0;
+}
