5b15cf95ba8321c16874442d4b1bc1b232d32127
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-08-15 11:00:37 +0600

    print_integer.cpp

---
+++ print_integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number; // Variable to store the input
+
+    cout << "Enter an integer: ";
+    cin >> number; // Taking input from the user
+
+    cout << "You entered: " << number << endl; // Printing the entered integer
+
+    return 0;
+}
