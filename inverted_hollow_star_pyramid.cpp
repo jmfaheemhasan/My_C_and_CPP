2a04f15b6f16927d4f84e87293d75adf65ae384f
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-08-16 21:37:46 +0600

    inverted_hollow_star_pyramid.cpp

---
+++ inverted_hollow_star_pyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = rows; i >= 1; i--) {
+        
+        for (int j = 0; j < rows - i; j++) {
+            cout << " ";
+        }
+        
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            if (k == 1 || k == 2 * i - 1 || i == rows)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
