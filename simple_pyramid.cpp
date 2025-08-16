78a68de4a9c2fd28cf7ac3b10e31eab43831b5b7
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-08-16 19:22:10 +0600

    simple_pyramid.cpp

---
+++ simple_pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        // Spaces
+        for (int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+        // Stars
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
